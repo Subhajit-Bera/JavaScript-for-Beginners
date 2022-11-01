dd91a59c3dcce4e5452de02e7db1d48baf1856f2
Subhajit <subhabera211@gmail.com>
2022-11-01 22:14:51 +0530

    Update 5_Functions.js

--- 5_Functions.js
+++ 5_Functions.js
@@ -17,3 +17,32 @@ bill("Subha",10000);
 // output:
 // Subha you have to pay Rs:10000
 // Thank you for visiting Subha
+
+
+//Function that retuns a value
+function sum(a,b,c){
+    return a+b+c;
+}
+
+//Calling the function
+let ans=sum(2,2,2);
+console.log(ans);
+
+
+//Function with default parameters
+function multiply(x,y=2){
+    return x*y;
+}
+
+//Calling Function :
+let retVal=multiply(6,6);
+console.log(retVal);
+//Op:36
+
+
+
+
+// If we do not pass the value of y, in that case default value of parameter y will be taken
+let val=multiply(6);
+console.log(val);
+//Op:12  (default value of y is 2)
