b55ced237394ae0956d70ce3227bc2d80865ea88
Subhajit <subhabera211@gmail.com>
2022-11-01 19:11:33 +0530

    Adding function code

---
+++ 5_Functions.js
@@ -0,0 +1,19 @@
+// Function Syntax:
+// function function_name(parameter1,parameter2,.....){
+    // code to be executed
+// }
+
+//This is a function which generate bill
+function bill(name,price){
+    console.log(name+" you have to pay Rs:"+price);
+    console.log("Thank you for visiting "+name);
+}
+  
+
+
+//Call the function bill
+bill("Subha",10000);
+
+// output:
+// Subha you have to pay Rs:10000
+// Thank you for visiting Subha
